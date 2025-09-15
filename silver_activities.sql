9752e825416440af962c86b3d7e7768f8771742d
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2025-09-15 13:04:00 -0400

    Update silver_activities.sql

--- silver_activities.sql
+++ silver_activities.sql
@@ -1,5 +1,5 @@
 CREATE or replace VIEW silver_activities AS
 select
     "Activity ID" as activity_id
-    ,TO_TIMESTAMP("Activity Date", 'Mon DD, YYYY, HH12:MI:SS PM') as activity_date
+    ,TO_TIMESTAMP("Activity Date", 'Mon DD, YYYY, HH12:MI:SS PM') as activity_date --convert from text to a timestamp
 FROM activities
